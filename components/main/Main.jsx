cf3e02e0e1dd2ae91f408fe67722582f3a249e8a
temevh <temehama@gmail.com>
2025-01-16 17:45:21 +0200

    asyncstorage tomfoolery

--- components/main/Main.jsx
+++ components/main/Main.jsx
@@ -1,7 +1,8 @@
 import { View, StyleSheet } from "react-native";
 import { SuccessButton, TaskImage, TaskText } from ".";
 import { useRouter } from "expo-router";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useFocusEffect } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Button } from "react-native-paper";
 
@@ -13,32 +14,22 @@ const Main = () => {
     router.push("/success");
   };
 
-  useEffect(() => {
-    const checkAsync = async () => {
-      try {
-        const hard = await AsyncStorage.getItem("hard");
-        const fun = await AsyncStorage.getItem("fun");
-        const again = await AsyncStorage.getItem("again");
-        const completed = await AsyncStorage.getItem("completed");
-        if (hard !== null) {
-          console.log("hard in main: ", JSON.parse(hard));
+  useFocusEffect(
+    useCallback(() => {
+      const checkAsync = async () => {
+        try {
+          const completed = await AsyncStorage.getItem("completed");
+          if (completed !== null && JSON.parse(completed) === true) {
+            setIsDone(true);
+            console.log("completed in main: ", completed);
+          }
+        } catch (e) {
+          console.log(e);
         }
-        if (fun !== null) {
-          console.log("fun in main: ", JSON.parse(fun));
-        }
-        if (again !== null) {
-          console.log("again in main: ", JSON.parse(again));
-        }
-        if (completed !== null) {
-          setIsDone(true);
-          console.log("completed in main: ", JSON.parse(completed));
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    };
-    checkAsync();
-  }, []);
+      };
+      checkAsync();
+    }, [])
+  );
 
   return (
     <View
